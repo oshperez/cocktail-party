91532ca09544b5f3dcd84b281edfd3d9f55dac2e
Osmani Hernandez <osmanihernandezperez@gmail.com>
2025-07-08 23:41:59 -0400

    create grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app-grid">
+      <aside className="column column-left">
+        <div className="grid-item">
+          <h2>Search History</h2>
+          <ul></ul>
+        </div>
+      </aside>
+
+      <main className="column column-center">
+        <div className="grid-item">
+          <h1>Cocktail Party 🍸</h1>
+        </div>
+      </main>
+
+      <aside className="column column-right">
+        <div className="grid-item">
+          <h2>Ban List</h2>
+          <ul></ul>
+        </div>
+      </aside>
+    </div>
+  );
 }
 
 export default App;
